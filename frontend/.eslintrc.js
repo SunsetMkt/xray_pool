3cbce03a1a46a148679797a804ac21c064e1ea2a
Myon <myon.cn@gmail.com>
2022-08-19 14:49:08 +0800

    chore(frontend): init

---
+++ frontend/.eslintrc.js
@@ -0,0 +1,8 @@
+module.exports = {
+  // https://eslint.org/docs/user-guide/configuring#configuration-cascading-and-hierarchy
+  // This option interrupts the configuration hierarchy at this file
+  // Remove this if you have an higher level ESLint config file (it usually happens into a monorepos)
+  root: true,
+
+  extends: ['@imyon/eslint-config-preset'],
+};
---
+++ frontend/.prettierrc.js
@@ -0,0 +1,3 @@
+module.exports = {
+  ...require('@imyon/eslint-config-prettier'),
+};
